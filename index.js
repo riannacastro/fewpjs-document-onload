980859062c5f89f972ad415c5a5993036d91a1f9
Rianna Castro <riannaxcastro@gmail.com>
2021-09-02 09:44:46 -1000

    commit tree f6c15eae2c49ff61a3c10e2b37492d20879e969f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const newText = document.getElementById("text");
+    newText.innerHTML = "This is really cool!"
+})
